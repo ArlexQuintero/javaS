19fdddece122be7be1591b85179b2e06801bd4d1
ArlexQuintero <arlex05112005@gmail.com>
2026-02-27 07:38:30 -0500

    validacion con DOM

--- regUsu/script/script.js
+++ regUsu/script/script.js
@@ -1,20 +1,54 @@
-function validarName(name){
-    const regex= /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
-    return regex.test(name)
-}
-function validarEmail(email) {
-    
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    
-    return regex.test(email);
-}
-function validarTel(){
-    const regex= /^\d{10}$/;
 
-    return regex.test(telefono);
-}
-function validarPassword(password){
-    const regex= /^(?=.*\d).{8,}$/;
+const ValidarRecueri =()=>{
+    const name = document.querySelector("#name").value.trim();
+    const email = document.querySelector("#email").value.trim();
+    const telefono = document.querySelector("#tel").value.trim();
+    const password = document.querySelector("#password").value.trim();
 
-    return regex.test(password);
+    const message = document.querySelector("#message");
+    const result = document.querySelector("#result");
+
+    message.innerHTML = "";
+    result.innerHTML = "";
+
+    let regexNombre = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+    let regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let regexTelefono = /^\d{10}$/;
+    let regexPassword = /^(?=.*\d).{8,}$/;
+
+    if (name === "" || email === "" || telefono ==="" || password ===""){
+        message.innerHTML = "❎Error: Todos los campos son obligatorios";
+        
+        return;
+    } 
+    if(!regexNombre.test(name)){
+        message.innerHTML = "❎Error: El nombre debe de solo tener letras"
+        
+        return;
+    }
+    if(!regexCorreo.test(email)){
+        message.innerHTML = "❎Error: El correo no esta en un formato valido"
+        
+        return;
+    }
+    if(!regexTelefono.test(telefono)){
+        message.innerHTML = "❎Error: El telefono solo debe de tener 10 digitos numericos"
+        
+        return;
+    }
+    if(!regexPassword.test(password)){
+        message.innerHTML = "❎Error: La contraseña deve de tener 8 caracteres y almenos un numero"
+        
+        return;
+    }
+    message.innerHTML = "✅ Registro exitoso";
+    result.innerHTML = `
+        <p><strong>Nombre:</strong> ${name}</p>
+        <p><strong>Correo:</strong> ${email}</p>
+        <p><strong>Teléfono:</strong> ${telefono}</p>
+    `;
 }
+const btnEnviar = document.querySelector("#btnEnviar");
+btnEnviar.addEventListener('click', () =>{
+    ValidarRecueri()
+})
